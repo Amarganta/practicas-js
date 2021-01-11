68b96b2d12d7d95f74ae833f25b38398aca1bdc3
Amaranta Gonzalez <amarantag28@gmail.com>
2021-01-11 16:21:18 -0300

    elimine el boton de refresh

--- ejercicios/api/backoffice/src/users/detail.js
+++ ejercicios/api/backoffice/src/users/detail.js
@@ -1,21 +1,20 @@
-const urlBase = 'https://ada-api-clase-86-default-rtdb.firebaseio.com'
+const urlBase = 'https://ada-practicas-default-rtdb.firebaseio.com/';
 
 const params = new URLSearchParams(window.location.search)
 // for (const param of params) {
 //     console.log(param)
 // }
 const id = params.get('name');
+//console.log(id)
 
 const mostrarInfo = (data) => {
-    const card = document.getElementsById('card');
+    const card = document.getElementById('card');
     const h5 = document.createElement('h5');
     const p = document.createElement('p');
     const pp = document.createElement('p');
-    
-    h5.innerHTML = data.user;
-    p.innerHTML =  data.email;
-    pp.innerHTML = data.password;
-    
+    h5.innerHTML = 'Usuario: ' + data.user;
+    p.innerHTML = 'Email: ' + data.email;
+    pp.innerHTML = 'Password: ' + data.password;
     card.appendChild(h5);
     card.appendChild(p);
     card.appendChild(pp);
@@ -23,10 +22,11 @@ const mostrarInfo = (data) => {
 
 const user = () => {
     fetch(urlBase + '/users/' + id + '.json')
-    .then(response => response.json())
-    .then(data => {
-        mostrarInfo(data)
-    })
-    .catch(error => console.log(error))
+
+        .then(response => response.json())
+        .then(data => {
+            mostrarInfo(data)
+        })
+        .catch(error => console.log(error))
 }
 user();
